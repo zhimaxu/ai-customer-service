65a946431d08997921c491d7766591aab9d52feb
zhimaxu <zhimaxu@foxmail.com>
2026-06-21 20:08:56 +0800

    chore: add global CSS variables and base theme system

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,7 @@ import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import router from './router'
+import './style.css'
 
 const app = createApp(App)
 const pinia = createPinia()
